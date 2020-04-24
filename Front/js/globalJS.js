b1f7ee33c8816033ff5d4be627a3443df08e3e1f
gregoryo <arthurgregorioleal@gmail.com>
2020-04-24 14:24:23 -0300

    Create Forms.css,new-room.html,my-rooms.html; Update my-profile

--- Front/js/globalJS.js
+++ Front/js/globalJS.js
@@ -1,2 +1,13 @@
+function injectFavicon(){
 
+    var element = document.getElementsByTagName('head')[0];
+        element.innerHTML +=
+    `
+    <link rel="shortcut icon" href="../assets/favicon.png" />
+    <meta name="theme-color" content="#5e2a53">
+    
+    `;
+    }
+ 
 /* Execution trace */
+injectFavicon();
